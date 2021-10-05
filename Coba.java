770895343cda3e57cfa1bd0b275d90d5b21f560d
pdp30120 <pradiptapryanda301203@gmail.com>
2021-10-05 15:12:54 +0700

    Coba-coba aja

---
+++ Coba.java
@@ -0,0 +1,6 @@
+public class Coba {
+    public static void main(String[] args){
+	    System.out.println("Halooo apa kabar");
+		System.out.println("Nama gw Pradipta Deska Pryanda");
+	}
+}	
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # praktek2
+Nama  : Pradipta Deska Pryanda 
+Kelas : D3IF 45-04
